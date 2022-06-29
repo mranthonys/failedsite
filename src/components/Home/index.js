a4e6168bd02075fbaa2d6c17857c93f3a7981157
Anthony <asecreti@maryhurst.org>
2022-06-28 21:28:12 -0400

    Letter Animation Attempt

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,33 @@
 import LogoTitle from '../../assets/images/logo-a.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import LetterAnimation from '../LetterAnimation'
+import {useState, useEffect} from 'react'
 const Home = () => {
-
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const firstLineArray = ['A', 'n', 't', 'h', 'o','n', 'y', ' ',
+                           'S', 'e', 'c', 'r', 'e', 't', 'i']
+    
+    const secondLineArray = ['W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+    useEffect(() => {
+      setTimeout(() => {
+       setLetterClass('text-animate-hover')
+      }, 4000)
+    }, [])
     return (
       <div className="container home-page">
         <div className="text-zone">
-            <h1>Hi, I'm Anthony Secreti<br />
+            <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i, </span>
+
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m </span>
+            <LetterAnimation letterClass={letterClass} strArray={firstLineArray} idx={15}/>
+              <br />
             <img src={LogoTitle} alt="developer" />
             <br/>
-            Web Developer
+            <LetterAnimation letterClass={letterClass} strArray={secondLineArray} idx={22} />
             </h1>
             <h2>Frontend Developer / JavaScript Enthusiast / Child At Heart</h2>
             <Link to="/contact" className='flat-button'>CONTACT ME</Link>
